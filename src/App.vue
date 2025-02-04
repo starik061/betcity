2877b4ed21556b391f19753835e40d06da86b694
starik061 <starik061@gmail.com>
2025-02-04 20:36:46 +0200

    add clg

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import { RouterLink, RouterView } from 'vue-router'
 
 export default {
   mounted() {
+    console.log("window.Telegram?.WebApp", window.Telegram?.WebApp)
+
     if (window.Telegram?.WebApp) {
       window.Telegram.WebApp.expand(); // Развернуть Web App
       if (window.Telegram.WebApp.requestFullscreen) {
