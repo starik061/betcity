233a9df6d3e94c36ad95714c0abdc04edaf051a0
starik061 <starik061@gmail.com>
2025-02-05 18:30:46 +0200

    test full screen

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,26 @@ export default {
     // ) {
     //   window.Telegram.WebApp.requestFullscreen();
     // }
-    if (
-      window.Telegram &&
-      window.Telegram.WebApp &&
-      window.Telegram.WebApp.platform &&
-      !window.Telegram.WebApp.platform === "unknown" &&
-      Number(window.Telegram.WebApp?.version) >= 8.0
-    ) {
-      window.Telegram.WebApp.expand();
-      // window.Telegram.WebApp.requestFullscreen();
-    }
+    // if (
+    //   window.Telegram &&
+    //   window.Telegram.WebApp &&
+    //   window.Telegram.WebApp.platform &&
+    //   !window.Telegram.WebApp.platform === "unknown" &&
+    //   Number(window.Telegram.WebApp?.version) >= 8.0
+    // ) {
+    //   window.Telegram.WebApp.expand();
+    //   // window.Telegram.WebApp.requestFullscreen();
+    // }
+    console.log("window.Telegram?.WebApp", window.Telegram?.WebApp)
 
+    if (window.Telegram?.WebApp) {
+      window.Telegram.WebApp.expand(); // Развернуть Web App
+      if (window.Telegram.WebApp.requestFullscreen) {
+        window.Telegram.WebApp.requestFullscreen(); // Запросить полный экран (если доступно)
+      }
+    }
   }
+
 }
 </script>
 
