6cde7a1e1b49395606500670e319d1b987aefc27
starik061 <starik061@gmail.com>
2025-02-08 20:38:37 +0200

    add tap octopus

--- src/views/GameOne.vue
+++ src/views/GameOne.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main-container game-one-container">
+    <img src="/img/tap-octopus.png" class="tap-octopus" alt="octopus for tap">
     <TopNavPanel class="top-panel-nav" />
 
     <ForecastDetails v-for="(item, index) in 2" :key="index" />
@@ -33,7 +34,19 @@ export default {
 
 <style lang="scss" scoped>
 .game-one-container {
+  position: relative;
   padding-top: 48px;
+  overflow: visible;
+}
+
+.tap-octopus {
+  position: absolute;
+  top: 55vh;
+  right: -20%;
+  width: 22.5%;
+  aspect-ratio: 1 / 1;
+  transform: translate(-50%, -50%);
+  z-index: 100;
 }
 
 .top-panel-nav {
