20ac3fa7341a6032f22590571cacab0a052e64ba
starik061 <starik061@gmail.com>
2025-02-14 18:25:16 +0200

    change to createWebHashHistory in router nd crete 404.html for right updating page in browser

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ import Profile from "../views/Profile.vue";
 import UnsupportedPlatform from "../views/UnsupportedPlatform.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
