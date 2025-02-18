377aa66e615f9370910dc0f34eb00a487b9f72c2
starik061 <starik061@gmail.com>
2025-02-18 10:36:34 +0200

    test of BackButton

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -147,6 +147,13 @@ export default {
     }
   },
 
+  mounted() {
+    window.Telegram.WebApp.BackButton.onClick(() => {
+      alert("Ану не трож кнопку!")
+    });
+    window.Telegram.WebApp.BackButton.show();
+  },
+
   methods: {
     ...mapActions(useAppStore, ['openModal', 'closeModal'])
   }
