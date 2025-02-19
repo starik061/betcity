db0c1a32abef1d41810e912e5e643b6ca4691277
starik061 <starik061@gmail.com>
2025-02-19 16:54:53 +0200

    remove clg

--- src/utils/fixBodyPadding.js
+++ src/utils/fixBodyPadding.js
@@ -2,5 +2,4 @@ export const fixBodyPadding = () => {
   const paddingValue = window.innerWidth - document.body.offsetWidth;
 
   document.body.style.paddingRight = paddingValue + "px";
-  console.log("first");
 };
