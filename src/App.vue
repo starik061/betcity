e922c0d73fa8bf2cdc6658ad0ae35f9430237390
starik061 <starik061@gmail.com>
2025-02-26 14:50:38 +0200

    change transition views duration

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@ export default {
 /* Анимация смены экрана справа налево */
 .slide-enter-active,
 .slide-leave-active {
-  transition: transform 0.2s ease-in-out, opacity 0.2s;
+  transition: transform 0.3s ease-in-out, opacity 0.3s;
 }
 
 .slide-enter {
