ae3b81acf1e69f6a2c521cdd8b61bebfee72c504
starik061 <starik061@gmail.com>
2025-03-06 17:26:10 +0200

    test of top nav panel top distance

--- src/components/TopNavPanel.vue
+++ src/components/TopNavPanel.vue
@@ -203,7 +203,7 @@ export default {
 <style lang="scss" scoped>
 .top-panel-container {
   // padding-top: calc(var(--tg-content-safe-area-inset-top) + 23px);
-  padding-top: 52px;
+  padding-top: 72px;
 
   // &.unusual-platform {
   // padding-top: 43px;
